af8363eadd31cee736f8e3d434f666aab26434eb
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-02-20 10:40:02 -0800

    footer added

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const burgerMenu = document.getElementById("check");
 const menuForm = document.querySelector(".mobile__menu");
+const navMenuOverlay = document.querySelector(".mob__nav__overlay");
+
 burgerMenu.addEventListener("change", function () {
   menuForm.classList.toggle("toggle__mobile--menu");
+  navMenuOverlay.classList.toggle("show");
+});
+
+navMenuOverlay.addEventListener("click", function () {
+  menuForm.classList.toggle("toggle__mobile--menu");
+  navMenuOverlay.classList.remove("show");
+  document.getElementById("check").checked = false;
 });
