a259aa7433666c5ae8ed3e2504b7d56ff5819e58
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-03-01 08:51:19 -0800

    cart modal added

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const burgerMenu = document.getElementById("check");
 const menuForm = document.querySelector(".mobile__menu");
 const navMenuOverlay = document.querySelector(".mob__nav__overlay");
+const cartClose = document.querySelector(".cart__close");
+const cartIcon = document.querySelector(".nav__icons__cart");
+const cart = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
 
 burgerMenu.addEventListener("change", function () {
   menuForm.classList.toggle("toggle__mobile--menu");
@@ -11,4 +15,19 @@ navMenuOverlay.addEventListener("click", function () {
   menuForm.classList.toggle("toggle__mobile--menu");
   navMenuOverlay.classList.remove("show");
   document.getElementById("check").checked = false;
+});
+
+cartIcon.addEventListener("click", function (e) {
+  cart.classList.toggle("cart--show");
+  cartOverlay.classList.toggle("cart-overlay--show");
+});
+
+cartClose.addEventListener("click", function () {
+  cart.classList.toggle("cart--show");
+  cartOverlay.classList.toggle("cart-overlay--show");
+});
+
+cartOverlay.addEventListener("click", function () {
+  cart.classList.toggle("cart--show");
+  cartOverlay.classList.toggle("cart-overlay--show");
 });
